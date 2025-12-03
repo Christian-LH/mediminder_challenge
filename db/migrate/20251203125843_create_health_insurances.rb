d703eb5f2a0d0f43b33af8727d59ebe60e58e287
Christian Lange-Hausstein <langehausstein@gmail.com>
2025-12-03 15:07:21 +0100

    Remove incorrect local migrations

--- db/migrate/20251203125843_create_health_insurances.rb
+++
@@ -1,9 +0,0 @@
-class CreateHealthInsurances < ActiveRecord::Migration[7.1]
-  def change
-    create_table :health_insurances do |t|
-      t.string :name
-
-      t.timestamps
-    end
-  end
-end
