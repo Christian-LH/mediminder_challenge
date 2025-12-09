0b8ab02bb9148ee07a036a5a13a1732bc212eced
Antonius Lambsdorff <lambsdorffa@gmail.com>
2025-12-09 23:06:29 +0100

    got the loader working

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -3,3 +3,15 @@ import "@hotwired/turbo-rails"
 import "controllers"
 import "@popperjs/core"
 import "bootstrap"
+
+
+document.addEventListener("turbo:load", () => {
+  const form   = document.querySelector('form[data-role="vaccination-form"]');
+  const loader = document.getElementById("vaccination-loader");
+
+  if (!form || !loader) return;
+
+  form.addEventListener("submit", () => {
+    loader.classList.remove("d-none");
+  });
+});
